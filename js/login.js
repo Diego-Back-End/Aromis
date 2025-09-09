eb6812e56d32c83972b710e90d6758981256ce04
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-09-09 19:24:00 -0300

    se agregaron comentarios

--- js/login.js
+++ js/login.js
@@ -1,16 +1,23 @@
+// Cuando el usuario intente "enviar" el formulario de login
 document.getElementById("loginForm").addEventListener("submit", function(e) {
+
+    // Evitamos que la página se recargue sola
     e.preventDefault();
 
+    // Guardamos lo que escribió el usuario en el email y la contraseña
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
+     // Buscamos en el localStorage el email y la contraseña guardados antes (cuando se registró)
     const storedEmail = localStorage.getItem("userEmail");
     const storedPassword = localStorage.getItem("userPassword");
 
+        // Si lo que escribió el usuario coincide con lo guardado
     if (email === storedEmail && password === storedPassword) {
         alert("Login exitoso");
         window.location.href = "perfil.html"; // Redirige al perfil
     } else {
+        //si no coincide, mostramos un error
         alert(" Credenciales incorrectas");
     }
 });
--- js/register.js
+++ js/register.js
@@ -1,30 +1,38 @@
+// Cuando se envíe el formulario de registro...
 document.getElementById("registerForm").addEventListener("submit", function(e) {
+    // Evita que la página se recargue
     e.preventDefault();
 
+    // Guardamos lo que el usuario escribió en los campos
     const email = document.getElementById("email").value.trim();
     const user = document.getElementById("user").value.trim();
     const password = document.getElementById("password").value.trim();
 
+    // Validamos el correo: debe tener "@" y "."
     if (!email.includes("@") || !email.includes(".")) {
         alert("Por favor ingresa un correo válido");
-        return;
+        return; // detenemos aquí
     }
 
+    // Validamos el nombre: mínimo 3 letras
     if (user.length < 3) {
         alert("El nombre de usuario debe tener al menos 3 caracteres");
         return;
     }
 
+    // Validamos la contraseña: mínimo 6 letras/números
     if (password.length < 6) {
         alert("La contraseña debe tener al menos 6 caracteres");
         return;
     }
 
-    // Guardar en LocalStorage
+    // Si pasa todas las validaciones, guardamos los datos en el navegador
     localStorage.setItem("userEmail", email);
     localStorage.setItem("userName", user);
     localStorage.setItem("userPassword", password);
 
+    // Avisamos que todo salió bien
     alert("Registro exitoso");
-    window.location.href = "login.html"; // redirige al login
+    // Lo mandamos a la página de login
+    window.location.href = "login.html"; 
 });
